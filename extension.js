37381286b45ad1787d79a4e0db61d37774c795b5
TMGMrChill <zelousdreamer4@gmail.com>
2015-03-07 07:54:20 -0600

    Update extension.js

--- extension.js
+++ extension.js
@@ -98,7 +98,7 @@
         themeLink: null,
         fbLink: null,
         youtubeLink: null,
-        website: null,
+        website: "www.tmgaming.org",
         intervalMessages: [],
         messageInterval: 5,
         songstats: true,
